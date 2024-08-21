44eddf07c0ca63f2edd3870397fd0a8d5f96131d
Juan Calzada <calzadaj@hotmail.es>
2024-08-21 11:35:34 +0200

    Add save to internal DB button.

--- WebClient/src/components/Import.tsx
+++ WebClient/src/components/Import.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FileInput, Label } from 'flowbite-react';
+import uploadSvg from '../assets/upload.svg';
+import './import.css'; // Import the CSS file
 
 interface ImportProps {
   // define your props here
@@ -17,20 +19,20 @@ const Import: React.FC<ImportProps> = (props) => {
   };
 
   return (
-    <div className="w-screen flex justify-center">
-      <div className="w-1/2 max-w-md">
+    <div className="import-container">
+      <div className="import-wrapper">
         <div>
-          <h1 className="text-center text-xl py-5">Import</h1>
+          <h1 className="import-header">Import</h1>
         </div>
-        <div className="flex flex-col text-center">
-          <div className="py-10">
+        <div className="import-content">
+          <div className="import-dropzone">
             <Label
               htmlFor="dropzone-file"
               className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
             >
               <div className="flex flex-col items-center justify-center pb-6 pt-5">
                 <svg
-                  className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+                  className="import-icon"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -44,11 +46,11 @@ const Import: React.FC<ImportProps> = (props) => {
                     d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                   />
                 </svg>
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <p className="import-instruction">
                   <span className="font-semibold">Click to upload</span> or drag
                   and drop
                 </p>
-                <p className="text-xs text-gray-500 dark:text-gray-400">
+                <p className="import-instruction-small">
                   [espData.json]-like file
                 </p>
               </div>
@@ -59,21 +61,28 @@ const Import: React.FC<ImportProps> = (props) => {
               />
             </Label>
           </div>
-          <div className="py-10">
-            <p>
-              {inputFile ? (
-                <span className="text-green-500 dark:text-green-400">
-                  {inputFile.name} uploaded
+          <div className="import-dropzone">
+            {inputFile ? (
+              <div
+                className="import-file-details"
+                onClick={() => {
+                  // Save file to internal DB
+                }}
+              >
+                <span className="import-file-info">
+                  Save {inputFile.name} to internal DB
                 </span>
-              ) : (
-                <span className="text-red-500 dark:text-red-400">
-                  No file uploaded
-                </span>
-              )}
-            </p>
+                <img
+                  src={uploadSvg}
+                  alt="uploadIcon"
+                  className="import-upload-icon"
+                />
+              </div>
+            ) : (
+              <span className="import-no-file">No file uploaded</span>
+            )}
           </div>
         </div>
-        <div className="text-center py-3"></div>
       </div>
     </div>
   );
